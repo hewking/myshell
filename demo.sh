5d27b727774d0d7102008c28f479c9515dbc17e3
jianhao <cjhcoder@qq.com>
2022-11-20 17:25:32 +0800

    feat: add database.sh

---
+++ demo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+. ./database.sh
+
+db_set 123456 '{"name":"London","attractions":["Big Ben","London Eye"]}'
+
+db_set 42 '{"name":"San Francisco","attractions":["Golden Gate Bridge"]}'
+
+db_get 42 
+# {"name":"San Francisco","attractions":["Golden Gate Bridge"]}
